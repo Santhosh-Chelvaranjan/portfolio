f6209e018bd963b12cbd31c7b4230b153e5c9d7f
Santhosh-Chelvaranjan <76189329+Santhosh-Chelvaranjan@users.noreply.github.com>
2021-02-06 22:23:04 -0500

    Added projects and contact page

--- port.js
+++ port.js
@@ -19,7 +19,7 @@ TxtType.prototype.tick = function() {
   this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
   var that = this;
-  var delta = 200 - Math.random() * 100;
+  var delta = 150 - Math.random() * 100;
 
 
 
